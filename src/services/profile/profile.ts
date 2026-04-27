83cb848a16df6eed904f8552383da7064d488407
Alekmeister <olegpro6@gmail.com>
2026-04-27 23:48:02 +0300

    continue

--- src/services/profile/profile.ts
+++ src/services/profile/profile.ts
@@ -1,21 +1,27 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ProfileInterface } from './profile.interface';
 import { baseApiUrl } from '@shared/api/constants';
 import { PaginationType } from '@shared/api/types';
-import { map } from 'rxjs';
+import { map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProfileService {
   http = inject(HttpClient);
+  me = signal<ProfileInterface | null>(null);
 
   getTestAccounts() {
     return this.http.get<ProfileInterface[]>(`${baseApiUrl}/account/test_accounts`);
   }
   getMe() {
-    return this.http.get<ProfileInterface>(`${baseApiUrl}/account/me`);
+    return (
+      this.http
+        .get<ProfileInterface>(`${baseApiUrl}/account/me`)
+        // tap позволяет "заглянуть" в поток и выполнить какое-то действие, не изменяя данные которые идут дальше.
+        .pipe(tap((res) => this.me.set(res)))
+    );
   }
 
   getSubscribersShortList() {
--- src/shared/ui/sidebar/sidebar/sidebar.ts
+++ src/shared/ui/sidebar/sidebar/sidebar.ts
@@ -1,21 +1,24 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { SvgIcon } from '@shared/ui/svg-icon/svg-icon';
 import { AsyncPipe, NgForOf } from '@angular/common';
 
 import { RouterLink } from '@angular/router';
 import { ProfileService } from '@services/profile/profile';
 import { SubscriberCard } from '@shared/ui/sidebar/subscriber-card/subscriber-card';
+import { firstValueFrom } from 'rxjs';
+import { ImgUrlsPipe } from '@shared/pipes/img-urls-pipe';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [SvgIcon, NgForOf, RouterLink, AsyncPipe, SubscriberCard],
+  imports: [SvgIcon, NgForOf, RouterLink, AsyncPipe, SubscriberCard, ImgUrlsPipe],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
-export class Sidebar {
+export class Sidebar implements OnInit {
   profileService = inject(ProfileService);
 
-  subscribers$ = this.profileService.getSubscribersShortList();
+  subscribers$ = this.profileService.getSubscribersShortList(); // $ Observable
+  me = this.profileService.me;
 
   menuItems = [
     {
@@ -34,4 +37,13 @@ export class Sidebar {
       link: 'search',
     },
   ];
+  // ngOnInit аля componentDidMount() или
+  //   useEffect(() => {
+  //       profileService.getMe();
+  //   }, [])
+
+  ngOnInit() {
+    // firstValueFrom без него не выполнится, или нужно подписаться. + в том, что не нужно будет управлять подпиской/отпиской
+    firstValueFrom(this.profileService.getMe());
+  }
 }
